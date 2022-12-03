bc70a97864e2c99d64c52a8df4dd7144970c375c
kang <rilac1@naver.com>
2022-12-04 07:33:08 +0900

    :sparkles: findByMember 로직 작성

---
+++ src/main/java/yapp/allround3/project/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package yapp.allround3.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import yapp.allround3.project.domain.Project;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
---
+++ src/main/java/yapp/allround3/project/service/ProjectService.java
@@ -0,0 +1,36 @@
+package yapp.allround3.project.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import yapp.allround3.member.domain.Member;
+import yapp.allround3.participant.domain.Participant;
+import yapp.allround3.participant.repository.ParticipantRepository;
+import yapp.allround3.project.domain.Project;
+import yapp.allround3.project.repository.ProjectRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ProjectService {
+
+	private final ProjectRepository projectRepository;
+	private final ParticipantRepository participantRepository;
+
+	@Transactional
+	public Project save(Project project) {
+		return projectRepository.save(project);
+	}
+
+	public List<Project> findByMember(Member member) {
+		List<Participant> participants = participantRepository.findByMember(member);
+
+		return participants.stream()
+			.map(Participant::getProject)
+			.collect(Collectors.toList());
+	}
+}

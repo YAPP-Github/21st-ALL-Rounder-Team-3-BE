99d8dbfe73dd1b581899d15461e0f957a479a07f
kang <rilac1@naver.com>
2022-12-04 07:36:12 +0900

    :sparkles: BaseTimeEntity 적용

---
+++ src/main/java/yapp/allround3/common/entity/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package yapp.allround3.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+	@CreatedDate
+	private LocalDateTime createdDate;
+
+	@LastModifiedDate
+	private LocalDateTime lastModifiedDate;
+}

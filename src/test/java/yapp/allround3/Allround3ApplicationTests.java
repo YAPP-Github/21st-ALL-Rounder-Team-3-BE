379c8dbdda21ce26595cf13160c615715250160e
ashlovesliitea <skyrider1811@gmail.com>
2022-12-04 01:43:04 +0900

    :white_check_mark: add test for successful ci progress

--- src/test/java/yapp/allround3/Allround3ApplicationTests.java
+++ src/test/java/yapp/allround3/Allround3ApplicationTests.java
@@ -3,11 +3,14 @@ package yapp.allround3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class Allround3ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void temporaryTest(){
+		assertThat(4-1).isEqualTo(3);
 	}
 
 }

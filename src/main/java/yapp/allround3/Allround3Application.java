961d3ccc9d0d28c99c2de5d7a2f098ca4953949e
kang <rilac1@naver.com>
2022-12-03 19:20:56 +0900

    :tada: Initial Commit

---
+++ src/main/java/yapp/allround3/Allround3Application.java
@@ -0,0 +1,13 @@
+package yapp.allround3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Allround3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Allround3Application.class, args);
+	}
+
+}
---
+++ src/test/java/yapp/allround3/Allround3ApplicationTests.java
@@ -0,0 +1,13 @@
+package yapp.allround3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Allround3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

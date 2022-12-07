54a44f794d8db8fa5a88df8fac417ed7c65242ea
ashlovesliitea <skyrider1811@gmail.com>
2022-12-08 02:21:25 +0900

    :recycle: lombok 사용

--- src/main/java/yapp/allround3/member/controller/dto/MemberResponse.java
+++ src/main/java/yapp/allround3/member/controller/dto/MemberResponse.java
@@ -1,19 +1,15 @@
 package yapp.allround3.member.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import yapp.allround3.member.domain.Member;
 
 @Data
+@AllArgsConstructor
 public class MemberResponse {
     private String email;
     private String nickname;
     private String imageUrl;
-
-    public MemberResponse(String email, String nickname, String imageUrl) {
-        this.email = email;
-        this.nickname = nickname;
-        this.imageUrl = imageUrl;
-    }
 
     public static MemberResponse of(Member member) {
         return new MemberResponse(member.getEmail(),

f2009c8fb135ebf7c88bdd0cfaf2f309457b805d
ashlovesliitea <skyrider1811@gmail.com>
2022-12-08 23:45:47 +0900

    :sparkles: member, participant 찾는 로직 추가

--- src/main/java/yapp/allround3/project/service/ProjectService.java
+++ src/main/java/yapp/allround3/project/service/ProjectService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import yapp.allround3.member.domain.Member;
+import yapp.allround3.member.repository.MemberRepository;
 import yapp.allround3.participant.domain.Participant;
 import yapp.allround3.participant.repository.ParticipantRepository;
 import yapp.allround3.project.domain.Project;
@@ -19,13 +20,22 @@ public class ProjectService {
 
 	private final ProjectRepository projectRepository;
 	private final ParticipantRepository participantRepository;
+	private final MemberRepository memberRepository;
 
 	@Transactional
 	public Project save(Project project) {
 		return projectRepository.save(project);
 	}
 
-	public List<Project> findByMember(Member member) {
+	public Member findMember(Long memberId){
+		return memberRepository.findMemberById(memberId);
+	}
+
+	public List<Participant> findParticipantsInProject(Project project){
+		return participantRepository.findParticipantsByProject(project);
+	}
+
+	public List<Project> findProjectByMember(Member member) {
 		List<Participant> participants = participantRepository.findByMember(member);
 
 		return participants.stream()

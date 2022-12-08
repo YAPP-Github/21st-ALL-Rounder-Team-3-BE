5a3b2482b300de0a18c8ab0a7150a3cc95695854
ashlovesliitea <skyrider1811@gmail.com>
2022-12-08 23:41:34 +0900

    :recycle: member 조회를 위한 fetch join 쿼리 추가

--- src/main/java/yapp/allround3/participant/repository/ParticipantRepository.java
+++ src/main/java/yapp/allround3/participant/repository/ParticipantRepository.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import yapp.allround3.member.domain.Member;
 import yapp.allround3.participant.domain.Participant;
+import yapp.allround3.project.domain.Project;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-
 	List<Participant> findByMember(Member member);
+
+	@Query("select p from Participant p join fetch p.member")
+	List<Participant> findParticipantsByProject(Project project);
 }

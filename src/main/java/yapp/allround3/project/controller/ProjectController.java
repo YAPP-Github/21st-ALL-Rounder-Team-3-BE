3543e8e5f068ae949faa7860ecbacc5f35381298
ashlovesliitea <skyrider1811@gmail.com>
2022-12-20 21:52:12 +0900

    :refactor: 메소드명 수정

--- src/main/java/yapp/allround3/project/controller/ProjectController.java
+++ src/main/java/yapp/allround3/project/controller/ProjectController.java
@@ -23,14 +23,14 @@ public class ProjectController {
 
     @ResponseBody
     @GetMapping("")
-    public CustomResponse<List<ProjectResponse>> findProjects(@RequestParam Long memberId){
+    public CustomResponse<List<ProjectResponse>> findProjectsByMember(@RequestParam Long memberId){
         // TODO : 임시방편으로 memberId받도록 설정했음, 추후에 jwt 로직 추가되면 거기서 꺼내서 쓰도록 할 예정.
         Member member = projectService.findMember(memberId);
         List<Project> projects = projectService.findProjectByMember(member);
         List<ProjectResponse> responses = projects.stream()
                 .map(project -> {
                     List<ParticipantDto> participantDtos = projectService
-                            .findParticipantsInProject(project).stream()
+                            .findParticipantsByProject(project).stream()
                             .map(ParticipantDto::of)
                             .toList();
                         return ProjectResponse.of(project, participantDtos);
@@ -41,11 +41,11 @@ public class ProjectController {
 
     @ResponseBody
     @GetMapping("")
-    public CustomResponse<ProjectResponse> findProjects(@PathVariable String projectId){
+    public CustomResponse<ProjectResponse> findProjectById(@PathVariable String projectId){
         //Project id 암호화 String -> Id 변환이 여기서 일어나는게 맞는지 약간 의문임?
         Project project = projectService.findProjectById(SecurityUtils.decodeKey(projectId));
         List<ParticipantDto> participantDtos = projectService
-                .findParticipantsInProject(project).stream()
+                .findParticipantsByProject(project).stream()
                 .map(ParticipantDto::of)
                 .toList();
         ProjectResponse response = ProjectResponse.of(project, participantDtos);

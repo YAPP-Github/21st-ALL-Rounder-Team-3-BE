3a170528662a09e9a2a52bddf93df0fc9146e45e
ashlovesliitea <skyrider1811@gmail.com>
2023-01-01 22:17:28 +0900

    :sparkles: FeedbackRepository 추가

---
+++ src/main/java/yapp/allround3/feedback/repository/FeedbackRepository.java
@@ -0,0 +1,15 @@
+package yapp.allround3.feedback.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import yapp.allround3.feedback.domain.Feedback;
+import yapp.allround3.task.domain.Task;
+
+import java.util.List;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback,Long> {
+
+
+}

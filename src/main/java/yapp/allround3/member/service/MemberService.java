8bea06eeed779a4fd97d7cbdfc66b7ce9e0e28ab
kang <rilac1@naver.com>
2023-01-07 18:05:13 +0900

    :necktie: Member 로직 수정

--- src/main/java/yapp/allround3/member/service/MemberService.java
+++ src/main/java/yapp/allround3/member/service/MemberService.java
@@ -1,23 +1,39 @@
 package yapp.allround3.member.service;
 
-
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import yapp.allround3.member.controller.dto.MemberResponse;
+
+import yapp.allround3.auth.oauth.Provider;
 import yapp.allround3.member.domain.Member;
 import yapp.allround3.member.repository.MemberRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
-@Transactional(readOnly=true)
 @RequiredArgsConstructor
+@Transactional(readOnly=true)
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    public MemberResponse findMemberById(Long id){
-        Member member= memberRepository.findMemberById(id);
-        return MemberResponse.of(member);
+    @Transactional
+    public Member join(Member member) {
+        Provider provider = member.getProvider();
+        String oauthId = member.getOauthId();
+
+        Optional<Member> optionalMember = memberRepository.findByProviderAndOauthId(provider, oauthId);
+
+        return optionalMember.orElseGet(() -> saveMember(member));
+    }
+
+    @Transactional
+    public Member saveMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public Member findMemberById(Long id){
+        return memberRepository.findById(id)
+            .orElseThrow();
     }
 }

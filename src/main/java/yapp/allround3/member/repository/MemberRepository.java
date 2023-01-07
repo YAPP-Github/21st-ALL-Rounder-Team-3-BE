76585fddebd87020aa1c87e26d385d95d69b506e
ashlovesliitea <skyrider1811@gmail.com>
2023-01-07 21:11:51 +0900

    :refactor: jwt 로직 반영

--- src/main/java/yapp/allround3/member/repository/MemberRepository.java
+++ src/main/java/yapp/allround3/member/repository/MemberRepository.java
@@ -1,15 +1,12 @@
 package yapp.allround3.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import yapp.allround3.member.domain.Member;
-import yapp.allround3.task.domain.Task;
 
-import java.util.List;
+
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    public Member findMemberById(Long id);
 
-    @Query("select f.participant.member from Feedback f where f.task=:task")
-    List<Member> findMembersGivenFeedback(Task task);
+
 }
--- src/main/java/yapp/allround3/member/service/MemberService.java
+++ src/main/java/yapp/allround3/member/service/MemberService.java
@@ -4,15 +4,16 @@ package yapp.allround3.member.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import yapp.allround3.common.exception.CustomException;
 import yapp.allround3.member.controller.dto.MemberResponse;
 import yapp.allround3.member.domain.Member;
 import yapp.allround3.member.repository.MemberRepository;
 import yapp.allround3.participant.domain.Participant;
 import yapp.allround3.participant.repository.ParticipantRepository;
 import yapp.allround3.project.domain.Project;
-import yapp.allround3.task.domain.Task;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly=true)
@@ -21,9 +22,9 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final ParticipantRepository participantRepository;
 
-    public MemberResponse findMemberById(Long id){
-        Member member= memberRepository.findMemberById(id);
-        return MemberResponse.of(member);
+    public Member findMemberById(Long id){
+        Optional<Member> member= memberRepository.findById(id);
+        return member.orElseThrow(()->new CustomException("존재하지 않는 멤버입니다."));
     }
 
     public List<Member> findMembersParticipatingInProject(Project project){
@@ -34,7 +35,4 @@ public class MemberService {
                 toList();
     }
 
-    public List<Member> findMembersGivenFeedback(Task task){
-        return memberRepository.findMembersGivenFeedback(task);
-    }
 }

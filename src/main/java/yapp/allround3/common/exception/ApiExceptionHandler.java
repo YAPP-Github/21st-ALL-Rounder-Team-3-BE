9179d2eb54c0f397eb5f534f074da82f59f7f2de
kang <rilac1@naver.com>
2023-02-05 21:01:31 +0900

    :sparkles: 400 Bad Request 추가

--- src/main/java/yapp/allround3/common/exception/ApiExceptionHandler.java
+++ src/main/java/yapp/allround3/common/exception/ApiExceptionHandler.java
@@ -1,14 +1,17 @@
 package yapp.allround3.common.exception;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import yapp.allround3.common.dto.CustomResponse;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
     @ExceptionHandler(CustomException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CustomResponse customExceptionHandler(CustomException e){
-     return CustomResponse.customError(e);
+        return CustomResponse.customError(e);
     }
 }

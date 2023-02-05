02d8c3e62aa30a2f24c6e58ecd7c0722019b4181
kang <rilac1@naver.com>
2023-02-05 23:13:56 +0900

    :ambulance: CORS 허용 설정

--- src/main/java/yapp/allround3/common/config/WebMvcConfig.java
+++ src/main/java/yapp/allround3/common/config/WebMvcConfig.java
@@ -23,6 +23,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*");
+			.allowedOrigins("*")
+			.allowedMethods("*");
 	}
 }

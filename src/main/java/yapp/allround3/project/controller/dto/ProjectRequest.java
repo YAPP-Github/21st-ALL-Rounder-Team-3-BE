a3753cdb7d34f3816d5ef1e569636efb43c5d408
ashlovesliitea <skyrider1811@gmail.com>
2023-02-05 19:43:10 +0900

    :sparkles: create/update project api

--- src/main/java/yapp/allround3/project/controller/dto/ProjectRequest.java
+++ src/main/java/yapp/allround3/project/controller/dto/ProjectRequest.java
@@ -3,18 +3,16 @@ package yapp.allround3.project.controller.dto;
 import java.time.LocalDate;
 
 import lombok.Data;
-import yapp.allround3.project.domain.Project;
+import yapp.allround3.project.domain.Difficulty;
+import yapp.allround3.project.domain.ProjectStatus;
 
 @Data
 public class ProjectRequest {
 
-	private String name;
-	private LocalDate startDate;
-	private LocalDate dueDate;
-
-	public ProjectRequest(Project project) {
-		this.name = project.getName();
-		this.startDate = project.getStartDate();
-		this.dueDate = project.getDueDate();
-	}
+    private String name;
+    private LocalDate startDate;
+    private LocalDate dueDate;
+    private String goal;
+    private Difficulty difficulty;
+	private ProjectStatus projectStatus;
 }

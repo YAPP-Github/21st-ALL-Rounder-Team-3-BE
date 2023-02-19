18877e902141a1415fa55b7554c4320ffe24b71a
ashlovesliitea <skyrider1811@gmail.com>
2023-02-19 21:24:54 +0900

    :sparkles: join시 participantCount 추가

--- src/main/java/yapp/allround3/participant/service/ParticipantService.java
+++ src/main/java/yapp/allround3/participant/service/ParticipantService.java
@@ -10,115 +10,118 @@ import yapp.allround3.feedback.repository.FeedbackRepository;
 import yapp.allround3.member.domain.Member;
 import yapp.allround3.member.service.MemberService;
 import yapp.allround3.participant.controller.dto.ParticipantFeedbackResponse;
-import yapp.allround3.participant.controller.dto.ParticipantResponse;
 import yapp.allround3.participant.domain.Participant;
 import yapp.allround3.participant.repository.ParticipantRepository;
 import yapp.allround3.project.domain.Project;
 import yapp.allround3.project.service.ProjectService;
 import yapp.allround3.task.domain.Task;
 import yapp.allround3.task.repository.TaskRepository;
-import yapp.allround3.task.service.TaskService;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ParticipantService {
 
-	private final MemberService memberService;
-	private final ParticipantRepository participantRepository;
-	private final ProjectService projectService;
-	private final FeedbackRepository feedbackRepository;
-	private final TaskRepository taskRepository;
+    private final MemberService memberService;
+    private final ParticipantRepository participantRepository;
+    private final ProjectService projectService;
+    private final FeedbackRepository feedbackRepository;
+    private final TaskRepository taskRepository;
 
-	public List<Participant> findParticipantsByProject(Project project) {
+    public List<Participant> findParticipantsByProject(Project project) {
 
-		return participantRepository.findParticipantsByProject(project);
-	}
+        return participantRepository.findParticipantsByProject(project);
+    }
 
-	public List<Participant> findParticipantsByProjectId(Long projectId) {
-		Project project = projectService.findProjectById(projectId);
+    public List<Participant> findParticipantsByProjectId(Long projectId) {
+        Project project = projectService.findProjectById(projectId);
 
-		return participantRepository.findParticipantsByProject(project);
-	}
+        return participantRepository.findParticipantsByProject(project);
+    }
 
-	public Participant findParticipantById(Long id) {
+    public Participant findParticipantById(Long id) {
 
-		return participantRepository.findParticipantById(id)
-			.orElseThrow(CustomException::new);
-	}
+        return participantRepository.findParticipantById(id)
+                .orElseThrow(()->new CustomException("해당 task의 참여자가 아닙니다."));
+    }
 
-	public int findParticipantCountByProject(Project project) {
+    public int findParticipantCountByProject(Project project) {
 
-		return participantRepository.countParticipantByProject(project);
-	}
+        return participantRepository.countParticipantByProject(project);
+    }
 
-	public List<Participant> findParticipantsGivenFeedback(Task task) {
+    public int findParticipantCountByProjectId(Long projectId) {
 
-		return participantRepository.findParticipantsGivenFeedback(task);
-	}
+        return participantRepository.countParticipantByProjectId(projectId);
+    }
 
-	@Transactional
-	public Participant joinProject(Long projectId, Long memberId) {
-		Member member = memberService.findMemberById(memberId);
-		Project project = projectService.findProjectById(projectId);
+    public List<Participant> findParticipantsGivenFeedback(Task task) {
 
-		Participant participant = Participant.from(project, member);
+        return participantRepository.findParticipantsGivenFeedback(task);
+    }
 
-		if (isExistedParticipant(participant)) {
-			throw new CustomException("이미 가입되어 있는 멤버에요.");
-		}
+    @Transactional
+    public Participant joinProject(Long projectId, Long memberId) {
+        Member member = memberService.findMemberById(memberId);
+        Project project = projectService.findProjectById(projectId);
 
-		return participantRepository.save(participant);
-	}
+        Participant participant = Participant.from(project, member);
 
-	@Transactional
-	public void changeLeader(Long memberId, Long participantId) {
-		Participant former = participantRepository.findById(participantId)
-			.orElseThrow(() -> new CustomException("해당 참여자가 존재하지 않습니다."));
+        if (isExistedParticipant(participant)) {
+            throw new CustomException("이미 가입되어 있는 멤버에요.");
+        }
 
-		Member member = memberService.findMemberById(memberId);
-		Project project = former.getProject();
-		Participant participant = participantRepository
-			.findParticipantByProjectAndMember(project, member)
-			.orElseThrow(() -> new CustomException("같은 프로젝트가 아닙니다."));
+        Participant savedParticipant = participantRepository.save(participant);
+        taskRepository.findTasksByProjectId(projectId)
+				.forEach(task -> taskRepository.updateTaskFeedbackRequiredPersonnel(task.getId()));
+        return savedParticipant;
+    }
 
-		participant.changeLeader(former);
-	}
+    @Transactional
+    public void changeLeader(Long memberId, Long participantId) {
+        Participant former = participantRepository.findById(participantId)
+                .orElseThrow(() -> new CustomException("해당 참여자가 존재하지 않습니다."));
 
-	public ParticipantFeedbackResponse findParticipantGroupByTask(Long taskId) {
-		Task task = taskRepository.findById(taskId)
-			.orElseThrow(() -> new CustomException("해당 테스크가 없습니다."));
+        Member member = memberService.findMemberById(memberId);
+        Project project = former.getProject();
+        Participant participant = participantRepository
+                .findParticipantByProjectAndMember(project, member)
+                .orElseThrow(() -> new CustomException("같은 프로젝트가 아닙니다."));
 
-		Participant participant = task.getParticipant();
-		Project project = participant.getProject();
+        participant.changeLeader(former);
+    }
 
-		List<Feedback> feedbacks = feedbackRepository.findByTask(task);
-		List<Participant> confirmedParticipants = feedbacks.stream()
-			.map(Feedback::getParticipant)
-			.toList();
+    public ParticipantFeedbackResponse findParticipantGroupByTask(Long taskId) {
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new CustomException("해당 테스크가 없습니다."));
 
-		List<Participant> unConfirmedParticipants = participantRepository.findParticipantsByProject(project);
-		unConfirmedParticipants.removeAll(confirmedParticipants);
-		unConfirmedParticipants.remove(participant);
+        Participant participant = task.getParticipant();
+        Project project = participant.getProject();
 
-		return ParticipantFeedbackResponse.of(confirmedParticipants, unConfirmedParticipants);
-	}
+        List<Feedback> feedbacks = feedbackRepository.findByTask(task);
+        List<Participant> confirmedParticipants = feedbacks.stream()
+                .map(Feedback::getParticipant)
+                .toList();
 
-	public Participant findParticipantByProjectAndMember(Project project, Member member) {
-		return participantRepository.findParticipantByProjectAndMember(project, member)
-			.orElseThrow(() -> new CustomException("프로젝트에 참여한 멤버가 아닙니다."));
-	}
+        List<Participant> unConfirmedParticipants = participantRepository.findParticipantsByProject(project);
+        unConfirmedParticipants.removeAll(confirmedParticipants);
+        unConfirmedParticipants.remove(participant);
 
-	private boolean isExistedParticipant(Participant participant) {
-		Member member = participant.getMember();
-		Project project = participant.getProject();
+        return ParticipantFeedbackResponse.of(confirmedParticipants, unConfirmedParticipants);
+    }
 
-		return participantRepository.findByMemberAndProject(member, project)
-			.isPresent();
-	}
+    public Participant findParticipantByProjectAndMember(Project project, Member member) {
+        return participantRepository.findParticipantByProjectAndMember(project, member)
+                .orElseThrow(() -> new CustomException("프로젝트에 참여한 멤버가 아닙니다."));
+    }
+
+    private boolean isExistedParticipant(Participant participant) {
+        Member member = participant.getMember();
+        Project project = participant.getProject();
+
+        return participantRepository.findByMemberAndProject(member, project)
+                .isPresent();
+    }
 }
--- src/main/java/yapp/allround3/task/repository/TaskRepository.java
+++ src/main/java/yapp/allround3/task/repository/TaskRepository.java
@@ -1,9 +1,9 @@
 package yapp.allround3.task.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import yapp.allround3.participant.domain.Participant;
-import yapp.allround3.project.domain.Project;
 import yapp.allround3.task.domain.Task;
 
 import java.util.List;
@@ -12,4 +12,11 @@ import java.util.Optional;
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findTasksByParticipant(Participant participant);
     Optional<Task> findTaskById(Long id);
+
+    @Modifying
+    @Query("update Task t set t.feedbackRequiredPersonnel=t.feedbackRequiredPersonnel+1 where t.id = :taskId")
+    void updateTaskFeedbackRequiredPersonnel(Long taskId);
+
+    @Query("select t from Task t where t.participant.project.id = :projectId")
+    List<Task> findTasksByProjectId(Long projectId);
 }

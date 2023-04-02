4ff5dfcaeda3f61513ebde0c8eccb614b626dd9d
William Kang <william@daangnpay.com>
2023-04-02 22:10:32 +0900

    :sparkles: 시큐리티 로직 제거

--- src/main/java/yapp/allround3/common/config/SecurityConfig.java
+++ src/main/java/yapp/allround3/common/config/SecurityConfig.java
@@ -2,62 +2,45 @@ package yapp.allround3.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.AuthenticatedPrincipalOAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import yapp.allround3.auth.oauth.handler.OAuth2AuthenticationFailureHandler;
-import yapp.allround3.auth.oauth.handler.OAuth2AuthenticationSuccessHandler;
-import yapp.allround3.auth.oauth.service.CustomOAuth2AuthService;
-import yapp.allround3.auth.oauth.service.CustomOidcUserService;
+import yapp.allround3.auth.jwt.AuthTokenProvider;
+import yapp.allround3.auth.jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final CustomOAuth2AuthService customOAuth2AuthService;
-	private final CustomOidcUserService customOidcUserService;
-	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-	private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+	private final AuthTokenProvider authTokenProvider;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-			.httpBasic().disable()
-			.formLogin().disable()
+		JwtAuthenticationFilter jwtAuthFilter = new JwtAuthenticationFilter(authTokenProvider);
+
+		http.authorizeHttpRequests()
+			.dispatcherTypeMatchers(HttpMethod.OPTIONS).permitAll()
+			// .antMatchers("/auth/**").permitAll() // filter permit가 안돼서 위에 제외 설정
+			.anyRequest().authenticated().and() // 해당 요청을 인증된 사용자만 사용 가능
+			.headers()
+			.frameOptions()
+			.sameOrigin().and()
+			.cors().and()
 			.csrf().disable()
-			.cors()
-			.configurationSource(corsConfigurationSource())
-			.and()
 			.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
-			.oauth2Login()
-			.userInfoEndpoint()
-			.oidcUserService(customOidcUserService)
-			.userService(customOAuth2AuthService)
-			.and()
-			.successHandler(oAuth2AuthenticationSuccessHandler)
-			.failureHandler(oAuth2AuthenticationFailureHandler);
+			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 
@@ -72,5 +55,28 @@ public class SecurityConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers(
+				"/**",
+
+				/* swagger v2 */
+				"/v2/api-docs",
+				"/swagger-resources",
+				"/swagger-resources/**",
+				"/configuration/ui",
+				"/configuration/security",
+				"/swagger-ui.html",
+				"/webjars/**",
+
+				/* swagger v3 */
+				"/v3/api-docs/**",
+				"/swagger-ui/**",
+
+				/* actuator */
+				"/actuator/**"
+		);
 	}
 }
